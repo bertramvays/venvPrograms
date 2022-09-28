6ad06064f63adfd22d81828b23912774355d572a
Dmitrij <grenko_d@protonmail.com>
2022-09-28 16:19:14 +0300

    #9.2 Переделал на классах.

--- lesson_009/02_log_parser.py
+++ lesson_009/02_log_parser.py
@@ -19,24 +19,39 @@
 # Входные параметры: файл для анализа, файл результата
 # Требования к коду: он должен быть готовым к расширению функциональности. Делать сразу на классах.
 
-input_log_file = 'events.txt'
-nok_events = {}
-with open(input_log_file, 'r', encoding='utf8') as file:
-    for line in file:
-        if 'NOK' in line:
-            s = line.split(' ')
-            nok_event_time = s[0][1:] + " " + s[1][:5]
-            if nok_event_time in nok_events:
-                nok_events[nok_event_time] += 1
-            else:
-                nok_events[nok_event_time] = 1
 
-otput_log_file = 'nok_events_per_minute.txt'
-with open(otput_log_file, 'w', encoding='utf8') as out_file:
-    for el in nok_events:
-        formated_string = f'[{el}] {nok_events[el]}\n'
-        out_file.write(formated_string)
+class ParseNokFile:
 
+    def __init__(self, input_log_file):
+        self.input_log_file = input_log_file
+        self.nok_events = {}
+
+    def get_nok_ivents(self):
+        with open(self.input_log_file, 'r', encoding='utf8') as file:
+            for line in file:
+                if 'NOK' in line:
+                    s = line.split(' ')
+                    self.nok_event_time = s[0][1:] + " " + s[1][:5]
+                    if self.nok_event_time in self.nok_events:
+                        self.nok_events[self.nok_event_time] += 1
+                    else:
+                        self.nok_events[self.nok_event_time] = 1
+        print("Nok events is selected.")
+        return self.nok_events
+
+    def write_nok_events_file(self, otput_log_file):
+        self.otput_log_file = otput_log_file
+        with open(self.otput_log_file, 'w', encoding='utf8') as self.out_file:
+            for el in self.nok_events:
+                formated_string = f'[{el}] {self.nok_events[el]}\n'
+                self.out_file.write(formated_string)
+        print(f"Nok file is created.")
+
+
+if __name__ == '__main__':
+    log_file = ParseNokFile('events.txt')
+    log_file.get_nok_ivents()
+    log_file.write_nok_events_file('Nok_events_per_minute.txt')
 
 # После выполнения первого этапа нужно сделать группировку событий
 #  - по часам
